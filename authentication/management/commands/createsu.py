f531376f366f30f7ba50fbb54f5635f648c49e76
Naman82 <b520031@iiit-bh.ac.in>
2023-07-22 19:31:15 +0530

    admin

--- authentication/management/commands/createsu.py
+++ authentication/management/commands/createsu.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(email='admin@mail.com').exists():
             User.objects.create_superuser(
-                email='admin@mail.com',
+                username='admin@mail.com',
                 password='Naman@2002'
             )
         print('Superuser has been created.')
